7b74c23ed661995e5e91fe439e3590827460c17a
Mina <minassgerges@gmail.com>
2024-12-12 18:58:44 +0200

    Added dummy friends to the friends

--- lib/services/FriendsService.dart
+++ lib/services/FriendsService.dart
@@ -1,7 +1,38 @@
 import '../models/User.dart';
 
 class FriendsService {
-  final List<User> _allFriends = [];
+  final List<User> _allFriends = [
+    User(
+      "Alice Johnson",
+      "1234567890",
+      "https://avatar.iran.liara.run/public/default_avatar.png",
+      1,
+    ),
+    User(
+      "Bob Smith",
+      "2345678901",
+      "https://avatar.iran.liara.run/public/default_avatar.png",
+      2,
+    ),
+    User(
+      "Charlie Brown",
+      "3456789012",
+      "https://avatar.iran.liara.run/public/default_avatar.png",
+      3,
+    ),
+    User(
+      "Diana Prince",
+      "4567890123",
+      "https://avatar.iran.liara.run/public/default_avatar.png",
+      4,
+    ),
+    User(
+      "Evan Wright",
+      "5678901234",
+      "https://avatar.iran.liara.run/public/default_avatar.png",
+      5,
+    ),
+  ];
 
   Future<List<User>> getFriends() async {
     await Future.delayed(const Duration(seconds: 2));
@@ -15,8 +46,8 @@ class FriendsService {
       User(
         name,
         phone,
-        "https://avatar.iran.liara.run/public",
-        _allFriends.length,
+        "https://avatar.iran.liara.run/public/default_avatar.png",
+        _allFriends.length + 1,
       ),
     );
   }
